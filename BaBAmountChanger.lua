02e832dec0b74f81f67de61b33dcbb94b707edf5
ls522 <71849703+ls522@users.noreply.github.com>
2020-10-10 19:39:08 +0200

    Update BaBAmountChanger.lua

--- BaBAmountChanger.lua
+++ BaBAmountChanger.lua
@@ -1,3 +1,6 @@
+Originally created by Sten
+
+
 Name.MouseButton1Down:connect(function()
 for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.BuildGui.InventoryFrame.ScrollingFrame.BlocksFrame:GetChildren()) do
     if v:FindFirstChild("AmountText") then
